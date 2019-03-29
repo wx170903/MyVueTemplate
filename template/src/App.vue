8e05d836ea7f9a9ad135beb76ba7aafb37c8c501
xianyili <384313798@qq.com>
2019-03-29 17:53:44 +0800

    status1.0

---
+++ template/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img src="./assets/mm.jpg">
+    {{#router}}
+    <router-view/>
+    {{else}}
+    <HelloWorld/>
+    {{/router}}
+  </div>
+</template>
+
+<script>
+{{#unless router}}
+import index from './components/index'
+
+{{/unless}}
+export default {
+  name: 'App'{{#router}}{{else}},
+  components: {
+    index
+  }{{/router}}
+}
+</script>
+<style lang="stylus" scoped>
+@import 'common/stylus/mixin'
+#app
+  width 100vw
+  height 100vh
+  text-align: center
+  .logo
+    margin-top: 200px
+</style>
---
+++ template/src/base/scroll.vue
@@ -0,0 +1,26 @@
+<template>
+ <div ref="wrapper-scroll">
+ </div>
+</template>
+
+<script>
+  import BScroll from 'better-scroll'
+ export default {
+   data () {
+     return {
+
+     }
+   },
+   methods: {
+
+   },
+   components: {
+
+   }
+}
+</script>
+
+<style scoped lang='stylus' rel='stylesheet/stylus'>
+
+ 
+</style>
